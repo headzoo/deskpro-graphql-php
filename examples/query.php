1d8f6de565a4766521cf3ac55fa91c4267a87702
headzoo <sean@headzoo.io>
2018-02-09 10:35:44 -0500

    Changes guzzle version

--- examples/query.php
+++ examples/query.php
@@ -7,15 +7,14 @@ $client = new GraphQL\Client('http://deskpro-dev.com');
 $client->setAuthKey(1, 'dev-admin-code');
 
 $query = '
-    query GetNews ($id: ID!) {
-        content_get_news(id: $id) {
-            title
-            content
-        }
+  query {
+    ticket_stars_all {
+        name
     }
+  }
 ';
 
 $data = $client->execute($query, [
     'id' => 1
 ]);
-print_r($data['content_get_news']);
+print_r($data);
